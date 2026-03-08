c1a207d0ccd0d744b1dba2eee362a2ff30930f9c
SaloEater <danil.lazarev@gmail.com>
2026-03-08 13:34:45 -0500

    fix crash

--- src/main/java/com/saloeater/curios_fix/mixin/SlotAttributeMixin.java
+++ src/main/java/com/saloeater/curios_fix/mixin/SlotAttributeMixin.java
@@ -5,22 +5,23 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.theillusivec4.curios.api.SlotAttribute;
 import top.theillusivec4.curios.mixin.CuriosImplMixinHooks;
 
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-@Mixin(CuriosImplMixinHooks.class)
-public abstract class CuriosImplMixinHooksMixin {
+@Mixin(SlotAttribute.class)
+public abstract class SlotAttributeMixin {
     @Shadow
-    private static Map<String, UUID> UUIDS;
+    private static Map<String, SlotAttribute> SLOT_ATTRIBUTES;
 
     @Inject(
             method = "<clinit>",
             at = @At("RETURN")
     )
     private static void clinit$curios_fix(CallbackInfo ci) {
-        UUIDS = new ConcurrentHashMap<String, UUID>();
+        SLOT_ATTRIBUTES = new ConcurrentHashMap<String, SlotAttribute>();
     }
 }
